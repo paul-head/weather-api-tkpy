53701774f0dfc6ee24345768d5b9a12de7fc9b1a
Pavel Golovanov <publichead@yandex.ru>
2021-02-01 13:19:53 +0300

    fastapi project structure.

---
+++ api/weather_api.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+import fastapi
+from fastapi import Depends
+
+from models.location import Location
+from services import openweather_service
+
+router = fastapi.APIRouter()
+
+
+@router.get('/api/weather/{city}')
+def weather(loc: Location = Depends(), units: Optional[str] = 'mertic'):
+
+    report = openweather_service.get_report(loc.city, loc.state, loc.country, units)
+
+    return report
