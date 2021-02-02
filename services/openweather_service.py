02bd1e82de5f58cea619b880f33ae5bd0a810ecb
Pavel Golovanov <publichead@yandex.ru>
2021-02-03 00:24:45 +0300

    add caching data with req

--- services/openweather_service.py
+++ services/openweather_service.py
@@ -1,10 +1,17 @@
 from typing import Optional
 import httpx
 
+from infrastructure import weather_cache
+
 api_key: Optional[str] = None
 
 
 async def get_report_async(city: str, state: Optional[str], country: str, units: str) -> dict:
+
+    forecast = weather_cache.get_weather(city, state, country, units)
+    if forecast:
+        return forecast
+
     if state:
         q = f'{city}, {country}'
     else:
@@ -16,5 +23,8 @@ async def get_report_async(city: str, state: Optional[str], country: str, units:
         resp.raise_for_status()
 
     data = resp.json()
-    print(data)
-    return data
+    forecast = data['main']
+
+    weather_cache.set_weather(city, state, country, units, forecast)
+
+    return forecast
